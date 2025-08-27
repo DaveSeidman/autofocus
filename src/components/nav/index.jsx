f352672eb999627768de23987b501d6d1d3a7cbf
Dave Seidman <daveseidman@Daves-MacBook-Pro.local>
2025-08-26 23:16:47 -0400

    styling

---
+++ src/components/nav/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './index.scss';
+
+
+export default function Nav() {
+
+
+  return (
+    <div className="nav">
+      <a href="/device">Device</a>
+      <a href="/apps">Apps</a>
+      <a href="/faqs">FAQs</a>
+      <a href="/contact">Contact</a>
+    </div>
+  )
+}
